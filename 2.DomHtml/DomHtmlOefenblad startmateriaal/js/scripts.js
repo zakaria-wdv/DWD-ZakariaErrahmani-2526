c345d60e2bb5656df530fac3c9c3254a3f3e0150
zakaria-wdv <zakariaerrahmani01@gmail.com>
2026-03-06 14:37:17 +0100

    les 2

---
+++ 2.DomHtml/DomHtmlOefenblad startmateriaal/js/scripts.js
@@ -0,0 +1,81 @@
+// === Oefening 1: querySelector() ===
+{
+	console.log('\n%c=== querySelector ===', 'color: #0c0');
+	const hdrTekoop = document.querySelector('#ex1-html h3');
+   // ... vul hier aan
+
+}
+
+// === Oefening 2: querySelectorAll() ===
+{
+	console.log('\n%c=== querySelectorAll ===', 'color: #0c0');
+   // ... vul hier aan
+
+}
+
+// === Oefening 3: addEventListener() ===
+{
+	console.log('\n%c=== addEventListener() ===', 'color: #0c0');
+
+   // declaraties
+   // ... vul hier aan
+
+	// event handler functies
+   // ... vul hier aan
+
+	// events koppelen
+   // ... vul hier aan
+}
+
+// === Oefening 4: innerHTML en innerText ===
+{
+	// DOBBELSTEEN OEFENING
+
+   // ... vul hier aan
+
+   // AANTAL TEKENS OEFENING
+
+   // ... vul hier aan
+}
+
+// === Oefening 5: HTML-attributen lezen en schrijven ===
+{
+   // ... vul hier aan
+
+}
+
+// === Oefening 6: validatie tekstinvoer ===
+{
+   // ALLEEN LETTERS EN SPATIES OEFENING
+
+   // ... vul hier aan
+
+   // WACHWOORD VALIDATIE OEFENING
+
+   // ... vul hier aan
+
+}
+
+// === Oefening 7: e.preventDefault() ===
+{
+   // ... vul hier aan
+
+}
+
+// === Oefening 8: e.target ===
+{
+   // ... vul hier aan
+
+}
+
+// === Oefening 9: event bubbling ===
+{
+   // ... vul hier aan
+
+}
+
+// === Oefening 10: formvalidatie ===
+{
+   // ... vul hier aan
+
+}
